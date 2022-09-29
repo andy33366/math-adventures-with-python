a05164b7a694b50e4f32ea2520599e1e251c02e6
andy33366 <114628588+andy33366@users.noreply.github.com>
2022-09-29 09:27:01 +0900

    makes a square with python turtle
    exersise 1-1 from the book

---
+++ turtletest.py
@@ -0,0 +1,23 @@
+from graphics import *
+from turtle import *
+
+def line():
+    forward(100)
+    shape('turtle')
+
+for i in range(4):
+    print(i)
+    line()
+    right(90)
+    
+
+#subprocess.run(["/usr/src/processing-4.0.1/processing"])
+
+#print("hello world")
+
+
+
+#displayArea=GraphWin('Rainbow Circle', 300, 300)
+
+#cir= Circle(Point(100,100), 20)
+#cir.draw(displayArea)
