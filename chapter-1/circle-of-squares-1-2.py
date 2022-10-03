2628c33efdaf9f121848ab7509274c13ff1ce872
andy33366 <andy704389@gmail.com>
2022-10-03 14:13:12 -0500

    added circle of squares to chapter 1 D dded

--- chapter-1/circle-of-squares-1-2.py
+++ chapter-1/circle-of-squares-1-2.py
@@ -7,4 +7,10 @@ def square():
     for i in range(4):
         forward(100)
         right(90)
-square()
+
+def sqcircle():
+    for i in range(60):
+        square()
+        right(5)
+
+sqcircle()
